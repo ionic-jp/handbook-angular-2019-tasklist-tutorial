50cbf0ebe883b0a8db54071f69065bdeafc9b337
榊原昌彦 <sakakibara@rdlabo.jp>
2019-11-06 17:41:33 +0900

    ステップ③　タスクの登録・表示と保存を行う

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,20 @@ export class HomePage {
     { name: 'タスク１' },
     { name: 'タスク２' },
   ];
+  task: string;
   constructor() {}
 
+  ionViewWillEnter() {
+    if ('tasks' in localStorage) {
+      this.tasks = JSON.parse(localStorage.tasks);
+    }
+  }
+
+  addTask() {
+    this.tasks.push({
+      name: this.task
+    });
+    localStorage.tasks = JSON.stringify(this.tasks);
+    this.task = '';
+  }
 }
